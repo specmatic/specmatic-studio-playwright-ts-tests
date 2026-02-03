4bf875662778cc47562134bcf9468f732980d2fb
Anand Bagmar <abagmar@gmail.com>
2026-02-03 16:02:56 +0530

    Refactor teardown script to use root directory for Docker script location

--- utils/teardown.js
+++ utils/teardown.js
@@ -4,9 +4,16 @@ module.exports = async () => {
   const envName = process.env.ENV_NAME || (process.env.CI ? "ci" : "local");
   if (process.env.USE_DOCKER === "true") {
     const isWindows = process.platform === "win32";
-    const dockerScript = isWindows ? "stop-docker.bat" : "./stop-docker.sh";
+    const path = require("path");
+    // Update to use the root directory for the script location
+    const rootDir = path.resolve(__dirname, "..");
+    const dockerScript = isWindows
+      ? path.join(rootDir, "stop-docker.bat")
+      : path.join(rootDir, "stop-docker.sh");
     try {
-      console.log(`[teardown] Stopping docker containers with ${dockerScript} (USE_DOCKER=true)`);
+      console.log(
+        `[teardown] Stopping docker containers with ${dockerScript} (USE_DOCKER=true)`,
+      );
       execSync(dockerScript, { stdio: "inherit" });
     } catch (e) {
       console.error("[teardown] Failed to stop docker:", e);
