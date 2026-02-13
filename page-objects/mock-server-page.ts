e8986fb275fe555ce79aebdcca760635a3739e90
Sushant092003 <bssushant2003@gmail.com>
2026-02-13 16:25:41 +0530

    Added Start and Stop mock server button

--- page-objects/mock-server-page.ts
+++ page-objects/mock-server-page.ts
@@ -1,20 +1,78 @@
 import { OpenAPISpecTabPage } from "./openapi-spec-tab-page";
-import { Locator, type TestInfo, Page } from "@playwright/test";
+import { Locator, type TestInfo, Page, expect } from "@playwright/test";
 import { BasePage } from "./base-page";
+import { PRODUCT_SEARCH_BFF_SPEC } from "../specs/specNames";
+import { takeAndAttachScreenshot } from "../utils/screenshotUtils";
 
 export class MockServerPage extends BasePage {
   private readonly openApiTabPage: OpenAPISpecTabPage;
 
   readonly specTree: Locator;
   readonly runMockServerTab: Locator;
+  private readonly specSection: Locator;
+  protected readonly mockPort: Locator;
+  public readonly mockToggleButton: Locator;
+  protected readonly infoMessageBox: Locator;
 
-  constructor(page: Page, testInfo: TestInfo, eyes?: any) {
-    super(page, testInfo, eyes);
+  constructor(page: Page, testInfo: TestInfo, eyes: any, specName: string) {
+    super(page, testInfo, eyes, specName);
     this.specTree = page.locator("#spec-tree");
-    this.runMockServerTab = page.getByText(/Run mock server/i);
+    this.specSection = page.locator(`div[id*="${specName}"]`);
+    this.runMockServerTab = this.specSection.locator('li[data-type="mock"]');
     this.openApiTabPage = new OpenAPISpecTabPage(this);
+    this.mockPort = this.specSection.locator("#mockPort");
+    this.mockToggleButton = this.specSection.locator(
+      'button[data-type="mock"]',
+    );
+    this.infoMessageBox = this.specSection.locator(".info-message-box");
   }
+
   async openRunMockServerTab() {
+    await this.gotoHome();
+    await this.sideBar.selectSpec(PRODUCT_SEARCH_BFF_SPEC);
+    await takeAndAttachScreenshot(this.page, "mock-tab-open");
     return this.openApiTabPage.openRunMockServerTab();
+  }
+
+  async fillMockPort(port?: number) {
+    await this.mockPort.clear();
+    if (port) {
+      await this.mockPort.fill(port.toString());
+      await takeAndAttachScreenshot(this.page, "entered Mock port");
+    }
+  }
+
+  async startMockServer() {
+    const isRunning = await this.mockToggleButton.getAttribute("data-running");
+    if (isRunning === "false") {
+      await this.mockToggleButton.click();
+      await takeAndAttachScreenshot(this.page, "mock-server-started");
+    }
+  }
+
+  async stopMockServer() {
+    const isRunning = await this.mockToggleButton.getAttribute("data-running");
+
+    if (isRunning === "true") {
+      await this.mockToggleButton.click();
+
+      await takeAndAttachScreenshot(this.page, "mock-server-stopped");
+    }
+  }
+
+  async getMockURL(): Promise<string> {
+    await expect(this.infoMessageBox).toBeVisible({ timeout: 10000 });
+
+    const fullText = await this.infoMessageBox.innerText();
+
+    const urlMatch = fullText.match(/https?:\/\/[^\s]+/);
+
+    if (!urlMatch) {
+      throw new Error(`Failed to extract URL from text: "${fullText}"`);
+    }
+
+    const rawUrl = urlMatch[0];
+
+    return rawUrl;
   }
 }
--- specs/openapi/run-mock-server/run-mock-server.spec.ts
+++ specs/openapi/run-mock-server/run-mock-server.spec.ts
@@ -2,17 +2,35 @@ import { test, expect } from "../../../utils/eyesFixture";
 import { PRODUCT_SEARCH_BFF_SPEC } from "../../specNames";
 import { MockServerPage } from "../../../page-objects/mock-server-page";
 
-test.describe("Mock Server", () => {
+test.describe("API Mocking", () => {
   test(
     "Run Mock Server for API Spec",
-    { tag: ["@mock"] },
+    { tag: ["@apiMocking"] },
     async ({ page, eyes }, testInfo) => {
-      const mockPage = new MockServerPage(page, testInfo, eyes);
+      const mockPage = new MockServerPage(
+        page,
+        testInfo,
+        eyes,
+        PRODUCT_SEARCH_BFF_SPEC,
+      );
 
       await test.step(`Go to Test page for Service Spec: '${PRODUCT_SEARCH_BFF_SPEC}'`, async () => {
-        await mockPage.gotoHomeAndOpenSidebar();
-        await mockPage.sideBar.selectSpec(PRODUCT_SEARCH_BFF_SPEC);
         await mockPage.openRunMockServerTab();
+      });
+      await test.step("Start Mock Server", async () => {
+        await mockPage.fillMockPort(9999);
+        await mockPage.startMockServer();
+        await expect(mockPage.mockToggleButton).toHaveAttribute(
+          "data-running",
+          "true",
+        );
+        await mockPage.getMockURL();
+
+        // await mockPage.stopMockServer();
+        // await expect(mockPage.mockToggleButton).toHaveAttribute(
+        //   "data-running",
+        //   "false",
+        // );
       });
     },
   );
