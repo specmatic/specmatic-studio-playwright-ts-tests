c62bc0aea5936fcf345c5d8fcb58d6d8db895eaa
Sushant092003 <bssushant2003@gmail.com>
2026-08-24 11:13:02 +0530

    Revert "feat: add navigation back from example after auto-fix"
    This reverts commit ee1ad49a9e2691beb85d201fdd4572e005bb0587.

--- page-objects/example-generation-page.ts
+++ page-objects/example-generation-page.ts
@@ -1298,7 +1298,6 @@ export class ExampleGenerationPage extends BasePage {
       await autoFixBtn.click();
       await takeAndAttachScreenshot(this.page, `clicked-auto-fix`, this.eyes);
       await this.verifyTitleAndCloseDialog("Fixed Example");
-      await this.goBackFromExample();
     });
   }
 
