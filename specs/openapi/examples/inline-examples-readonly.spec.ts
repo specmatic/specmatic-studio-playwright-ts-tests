d2379abfdbd1dcbf61a08610755003ed1f25e8e7
Sushant092003 <bssushant2003@gmail.com>
2026-08-24 12:21:57 +0530

    fix: add back button click from example details page

--- specs/openapi/examples/inline-examples-readonly.spec.ts
+++ specs/openapi/examples/inline-examples-readonly.spec.ts
@@ -57,6 +57,7 @@ test.describe("OpenAPI inline examples", () => {
       await examplesPage.assertInlineFixActionVisible();
 
       await examplesPage.fixExampleWithAutoFix();
+      await examplesPage.goBackFromExample();
       await examplesPage.assertInlineValidationState("CREATE_INVALID");
       await expect
         .poll(() => readSpecFile(), { timeout: 15000 })
